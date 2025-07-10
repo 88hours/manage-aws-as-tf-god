13b60eaba192bdd8b1fb4c07da0ffc78e2229c81
Nomi <itsnomihere@gmail.com>
2025-07-10 23:02:29 +1000

    .gitignore added

---
+++ managment-account-tf/create-aws-org-admin.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_user" "org_admin" {
+  name = "org-admin"
+}
+
+resource "aws_iam_user_policy" "org_admin_policy" {
+  name = "OrgAdminFullAccess"
+  user = aws_iam_user.org_admin.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "organizations:*",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_login_profile" "org_admin_login" {
+  user    = aws_iam_user.org_admin.name
+  pgp_key = "keybase:<your-keybase-username>" # or omit for unencrypted password
+}
+
+resource "aws_iam_access_key" "org_admin_key" {
+  user = aws_iam_user.org_admin.name
+}

3ac5d4d59e2b0b7547eaa1b5ecb21377941c7434
Nomi <itsnomihere@gmail.com>
2025-07-11 13:55:55 +1000

    Fix the terraform profile issue with HCP Cloud

--- main.tf
+++ main.tf
@@ -1,5 +1,24 @@
+terraform { 
+  #cloud { 
+  #  organization = "88Hours" 
+  #  workspaces { 
+  #    name = "88HoursTerraformMigration-Prod" 
+  #  } 
+  #} 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.3.0"
+    }
+  }
+}
+variable "aws_profile" {
+  description = "The AWS credentials profile name"
+  type        = string
+}
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  profile = var.aws_profile
 }
 variable "org_admin" {
   description = "The name of the IAM user"

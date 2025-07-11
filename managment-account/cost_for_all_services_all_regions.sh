57f7a6492331779bce70b13db75bfc4436c67b90
Nomi <itsnomihere@gmail.com>
2025-07-11 13:13:57 +1000

    Fix dos2unix format

--- managment-account/cost_for_all_services_all_regions.sh
+++ managment-account/cost_for_all_services_all_regions.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 PROFILE=$1
-
 if [[ -z "$PROFILE" ]]; then
   echo "Usage: $0 <aws-profile-name>"
   exit 1
 fi
+
 if ! aws sts get-caller-identity --profile "$PROFILE" &>/dev/null; then
   echo "Profile '$PROFILE' does not exist or is not configured correctly."
   exit 1
 fi
 
-# Detect OS and # Set time range (1 day before yesterday)
+# Detect OS and set time range (1 day before yesterday)
 if date -v -1d &>/dev/null; then
   # macOS / BSD-style
   END_DATE=$(date -v -1d +%Y-%m-%d)
@@ -20,16 +20,17 @@ else
   END_DATE=$(date +%Y-%m-%d)
   START_DATE=$(date -d "$END_DATE - 7 days" +%Y-%m-%d)
 fi
+
 echo "Cost for all services in all regions from $START_DATE to $END_DATE:"
-# Run Cost Explorer query
+
 aws ce get-cost-and-usage \
   --time-period Start=$START_DATE,End=$END_DATE \
   --granularity DAILY \
   --metrics "UnblendedCost" \
   --group-by Type=DIMENSION,Key=SERVICE \
-  --profile $PROFILE  \
   --region us-east-1 \
-  | jq -r '
+  --profile "$PROFILE" \
+| jq -r '
     .ResultsByTime[] |
     .TimePeriod.Start as $date |
     .Groups[] |

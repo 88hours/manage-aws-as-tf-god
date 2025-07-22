f4e984ad03baf5e7750725fde33d437d1924056c
Nomi <itsnomihere@gmail.com>
2025-07-22 20:31:29 +1000

    Create ecs_task_execution_role_attachment to be created by 88HoursSSODeveloper

--- modules/org-admin/sso-users/main.tf
+++ modules/org-admin/sso-users/main.tf
@@ -72,3 +72,23 @@ resource "aws_ssoadmin_account_assignment" "assign_devs" {
   target_type        = "AWS_ACCOUNT"
 
 }
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
